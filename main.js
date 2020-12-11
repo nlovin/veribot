7fe5d423131c379285250773149344a371ab8c78
Nathan Lovin <nlovin@umd.edu>
2020-12-10 21:37:56 -0500

    basic bot commands (one file)

--- main.js
+++ main.js
@@ -8,12 +8,20 @@ const {
 } = require('./config.json');
 
 
-
 client.once('ready', () => {
     console.log('Veribot is online!')
-})
+});
 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
 
+    const args = message.content.slice(prefix.length).split("/ +/");
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    }
+});
 
 
 client.login(token);
